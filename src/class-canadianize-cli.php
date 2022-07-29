2040400a6a4b17bc0621da7572107286317080bc
Lisa Schuyler <lisa@automattic.com>
2022-07-29 15:32:37 -0800

    Got the CLI to work!
    Will continue to refine, as I sit here beside an Alaskan river, working offline.

--- src/class-canadianize-cli.php
+++ src/class-canadianize-cli.php
@@ -7,7 +7,8 @@
  */
 
 declare( strict_types=1 );
-
+//use WP_CLI;
+//use WP_CLI\Utils;
 namespace Canadianize;
 
 /**
@@ -19,35 +20,42 @@ namespace Canadianize;
  * @author  Lisa Schuyler
  *
  */
-class Generate_Posts ( $args ) extends Canadianize {
+class Canadianize_Cli extends Canadianize {
 
-// Get Post Details.
-	$generate_this_number_of_posts = (int) $args[0];
-	$author_id                     = (int) $args[1]; // Id of author who to assign generated post to.
+	public function __invoke( $args ) {
+		// Get Post Details.
+		$generate_this_number_of_posts = (int) $args[0];
+		$author_id                     = (int) $args[1]; // Id of author who to assign generated post to.
 
-	$progress = \WP_CLI\Utils\make_progress_bar( 'Generating Posts', $generate_this_number_of_posts );
+		$progress = \WP_CLI\Utils\make_progress_bar( 'Generating Posts', $generate_this_number_of_posts );
 
-	for ( $i = 0; $i < $generate_this_number_of_posts; $i ++ ) {
+		for ( $i = 0; $i < $generate_this_number_of_posts; $i ++ ) {
 
-		$new_post = array(
-			'post_title'   => new Make_Content( 1, 1 ),
-			'post_status'  => 'publish',
-			'post_author'  => $author_id,
-			'post_type'    => 'post',
-			'post_content' => new Make_Content( 3, 10 ),
-		'tags_input'  => [ 'generated' , 'Canada', new Verb, new Noun, new Place],
-			//'meta_input'  => $assoc_args, // Simply passes all key value pairs to posts generated that can be used in testing.
-		);
+			$title        = new Make_Content( 1, 1 );
+			$post_content = new Make_Content( 3, 10 );
 
-		// Insert the post into the database.
-		wp_insert_post( $new_post );
+			$new_post = array(
+				'post_title'   => substr( (string) $title, 0, strpos( (string) $title, ' ', 100 ) ),
+				'post_status'  => 'publish',
+				'post_author'  => $author_id,
+				'post_type'    => 'post',
+				'post_content' => (string) $post_content,
+				'tags_input'   => [ 'generated', 'Canada', new Verb, new Noun, new Place ],
+				//'post_category' => new Noun,  //needs array of category IDs
+			);
 
-		$progress->tick();
+			// Insert the post into the database.
+			wp_insert_post( $new_post );
+
+			$progress->tick();
+		}
+
+		$progress->finish();
+
+		\WP_CLI::success( $generate_this_number_of_posts . ' posts generated!' ); // Prepends Success to message
+
 	}
-
-	$progress->finish();
-	\WP_CLI::success( $generate_this_number_of_posts . ' posts generated!' ); // Prepends Success to message
 }
 
+\WP_CLI::add_command( 'canadianize', 'Canadianize\Canadianize_Cli' );
 
-}
