69a46e66bb77d6ce9e4de44ea44dd0b63300e495
Lisa Schuyler <lisa@automattic.com>
2022-08-03 13:30:48 -0700

    Organize into methods
    Start organizing the pieces of the post into their own class methods.

--- src/class-canadianize-cli.php
+++ src/class-canadianize-cli.php
@@ -22,6 +22,43 @@ namespace Canadianize;
  */
 class Canadianize_Cli extends Canadianize {
 
+	private function generate_title(): string {
+		$title = (string) new Make_Content( 1, 1 );
+		if ( strlen( $title ) > 100 ) {
+			$title = substr( $title, 0, strpos( $title, ' ', 100 ) );
+		}
+
+		return $title;
+	}
+
+	private function generate_tags(): array {
+		$tags = array( "generated", "Canada" );
+		array_push( $tags, (string) new Verb, (string) new Park, (string) new Food, (string) new Vehicle );
+
+		return $tags;
+	}
+
+	private function generate_post_content(): string {
+		return (string) new Make_Content( 3, 10 );
+	}
+
+	private function generate_category(): array {
+		// Make a Canadianize category - check if it exists first.
+		$category       = (string) new Noun;
+		$category_array = term_exists( $category, 'category' );
+		if ( is_null( $category_array ) || ( $category_array === 0 ) ) {
+			// Then it doesn't exist, so let's add it.
+			$category_array = wp_insert_term( $category, 'category' );
+			if ( is_wp_error( $category_array ) ) {
+				error_log( print_r( "*** Oh no! Category wasn't added!?", true ) );
+			}
+			//$category_id = $category_id['term_id'];
+		}
+
+		return $category_array;
+
+	}
+
 	public function __invoke( $args ) {
 		// Get Post Details.
 		$generate_this_number_of_posts = (int) $args[0];
@@ -31,17 +68,15 @@ class Canadianize_Cli extends Canadianize {
 
 		for ( $i = 0; $i < $generate_this_number_of_posts; $i ++ ) {
 
-			$title        = new Make_Content( 1, 1 );
-			$post_content = new Make_Content( 3, 10 );
-
+			// Make a new post.
 			$new_post = array(
-				'post_title'   => substr( (string) $title, 0, strpos( (string) $title, ' ', 100 ) ),
-				'post_status'  => 'publish',
-				'post_author'  => $author_id,
-				'post_type'    => 'post',
-				'post_content' => (string) $post_content,
-				'tags_input'   => [ 'generated', 'Canada', new Verb, new Noun, new Place ],
-				//'post_category' => new Noun,  //needs array of category IDs
+				'post_title'    => $this->generate_title(),
+				'post_status'   => 'publish',
+				'post_author'   => $author_id,
+				'post_type'     => 'post',
+				'post_content'  => $this->generate_post_content(),
+				'tags_input'    => $this->generate_tags(),
+				'post_category' => $this->generate_category(),
 			);
 
 			// Insert the post into the database.
